8569c74d3f262271cdf4128abaeba2608661bf33
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-07 12:13:30 -0300

    005

---
+++ 005.js
@@ -0,0 +1,18 @@
+
+let result = 20
+do {
+  if (
+    result % 20 === 0 &&
+    result % 19 === 0 &&
+    result % 18 === 0 &&
+    result % 17 === 0 &&
+    result % 16 === 0 &&
+    result % 14 === 0 &&
+    result % 13 === 0 &&
+    result % 11 === 0
+  ) {
+    break
+  }
+  result += 20
+} while (true)
+console.log(result)
