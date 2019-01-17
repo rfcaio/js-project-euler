4f5752ff6b218a7354f8f1c401404c59e29923d2
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-17 12:29:05 -0300

    refactor 003

--- 003.js
+++ 003.js
@@ -1,22 +1,9 @@
+const { is_prime } = require('./utils')
 
-const isPrime = n => {
-  if (n === 2) {
-    return true
-  }
-  if (n % 2 === 0) {
-    return false
-  }
-  for (let i = 3, ii = n / 2; i < ii; i += 2) {
-    if (n % i === 0) {
-      return false
-    }
-  }
-  return true
-}
-const maxPrimeFactor = n => {
+const max_prime_factor_of = n => {
   let factor = 2
   do {
-    if (isPrime(factor) && n % factor === 0) {
+    if (is_prime(factor) && n % factor === 0) {
       n /= factor
     } else {
       factor += 1
@@ -24,4 +11,4 @@ const maxPrimeFactor = n => {
   } while (n !== 1)
   return factor
 }
-console.log(maxPrimeFactor(600851475143))
+console.log(max_prime_factor_of(600851475143))
