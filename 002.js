32f235edb7b1417c609b6f428acb6d62444a1f42
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-30 12:42:05 -0300

    refactor 002

--- 002.js
+++ 002.js
@@ -1,11 +1,15 @@
 
-const fib = n => (n === 1 || n === 2) ? n : fib(n - 1) + fib(n - 2)
-let element = null
+function fibonacci (n) {
+  return (n === 1 || n === 2) ? n : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
 let i = 1
-let sum = 0
-do {
-  element = fib(i)
-  sum += element % 2 === 0 ? element : 0
-  i++
-} while (element < 4000000)
-console.log(sum)
+let result = 0
+while (true) {
+  let element = fibonacci(i++)
+  if (element > 4000000) {
+    break
+  }
+  result += element % 2 === 0 ? element : 0
+}
+console.log(result)
