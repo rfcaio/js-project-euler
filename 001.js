08a982e166159b4789dba9affcdc3e6ac0b64125
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-30 12:32:09 -0300

    refactor 001

--- 001.js
+++ 001.js
@@ -1,9 +1,6 @@
 
-const get_sum_of_multiples = () => {
-  let sum = 0
-  for (let i = 3; i < 1000; i += 1) {
-    sum += i % 3 === 0 || i % 5 === 0 ? i : 0
-  }
-  return sum
+let result = 0
+for (let i = 3; i < 1000; i += 1) {
+  result += i % 3 === 0 || i % 5 === 0 ? i : 0
 }
-console.log(get_sum_of_multiples())
+console.log(result)
