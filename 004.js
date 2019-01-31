dea5f54421753d344d85d61ed16733e1a85b324e
Caio Rodrigues <rfcaio90@gmail.com>
2019-01-31 13:13:29 -0300

    refactor 004

--- 004.js
+++ 004.js
@@ -1,16 +1,14 @@
 
-const is_palindromic = number => {
+function is_palindromic (number) {
   return number == number.toString().split('').reverse().join('')
 }
-const get_largest_palindrome = () => {
-  let result = 0
-  for (let i = 100; i < 999; i += 1) {
-    for (let j = 100; j < 999; j += 1) {
-      if (is_palindromic(i * j) && result < i * j) {
-        result = i * j
-      }
+
+let result = 0
+for (let i = 100; i < 999; i += 1) {
+  for (let j = 100; j < 999; j += 1) {
+    if (is_palindromic(i * j) && result < i * j) {
+      result = i * j
     }
   }
-  return result
 }
-console.log(get_largest_palindrome())
+console.log(result)
