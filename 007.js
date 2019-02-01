1c421079b5317180962d072768870a7b5ad29b49
Caio Rodrigues <rfcaio90@gmail.com>
2019-02-01 13:09:41 -0300

    refactor 007

--- 007.js
+++ 007.js
@@ -1,24 +1,25 @@
 
-const isPrime = n => {
-  if (n === 2) {
+function is_prime (number) {
+  if (number === 2) {
     return true
   }
-  if (n % 2 === 0) {
+  if (number % 2 === 0) {
     return false
   }
-  for (let i = 3, ii = n / 2; i < ii; i += 2) {
-    if (n % i === 0) {
+  for (let i = 3, ii = number / 2; i < ii; i += 2) {
+    if (number % i === 0) {
       return false
     }
   }
   return true
 }
-let i = 2
-let count = 0
+
+let i = 0
+let result = 2
 while (true) {
-  if (isPrime(i) && ++count === 10001) {
+  if (is_prime(result) && ++i === 10001) {
     break
   }
-  i += 1
+  result += 1
 }
-console.log(i)
+console.log(result)
